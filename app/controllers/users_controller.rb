6d70ea03b26054ee246bd3d745124f3641ae9de1
Redx311 <redx318@gmail.com>
2016-04-13 15:27:53 +0000

    commit files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,8 +21,30 @@ class UsersController < ApplicationController
       render 'new'
 
     end
+  end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+     @user = User.find(params[:id])
+
+     if @user.update(user_params)
+
+     flash[:success] = "Your account was updated successfully"
+
+     redirect_to articles_path
+
+    else
+
+      render 'edit'
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 
     private
 
