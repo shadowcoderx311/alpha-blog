702d20fc4b5671e98fb7304ef4fa0f314d7394d2
Redx311 <redx318@gmail.com>
2016-04-18 14:53:25 +0000

    commit files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,60 +1,56 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:edit, :update, :show]
+    before_action :require_same_user, only: [:edit, :update]
 
-  def index
-    @users = User.paginate(page: params[:page], per_page: 5)
-  end
-
-    def new
-
-      @user = User.new
-
+    def index
+     @users = User.paginate(page: params[:page], per_page: 5)
     end
 
-    def create
+    def new
+    @user = User.new
+    end
 
-      @user = User.new(user_params)
-
+   def create
+    @user = User.new(user_params)
     if @user.save
+    flash[:success] = "Welcome to the alpha blog #{@user.username}"
+    redirect_to articles_path
 
-      flash[:success] = "Welcome to the alpha blog #{@user.username}"
+    else
+    render 'new'
+    end
+   end
 
+    def edit
+    end
+
+   def update
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
       redirect_to articles_path
 
     else
-
-      render 'new'
-
-    end
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-     @user = User.find(params[:id])
-
-     if @user.update(user_params)
-
-     flash[:success] = "Your account was updated successfully"
-
-     redirect_to articles_path
-
-    else
-
       render 'edit'
     end
-  end
+   end
 
-  def show
-    @user = User.find(params[:id])
-    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
-  end
+    def show
+      @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+    end
 
     private
-
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def require_same_user
+      if current_user != @user
+        flash[:danger] = "You can only edit your own account"
+        redirect_to root_path
     end
 
 end
