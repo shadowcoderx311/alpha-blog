39a1a4f9371714e6c5345baae0c9799a40397955
Redx311 <redx318@gmail.com>
2016-04-19 15:09:05 +0000

    commit files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,14 +13,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the alpha blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
   end
+
   def edit
   end
+
   def update
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
